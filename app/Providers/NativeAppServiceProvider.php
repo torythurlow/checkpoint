eda4508ccfc79b3380ce7f3a43afea644cce6e61
torythurlow <156625785+torythurlow@users.noreply.github.com>
2026-09-10 13:57:04 +0100

    Working version of NativePHP

---
+++ app/Providers/NativeAppServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Native\Desktop\Contracts\ProvidesPhpIni;
+use Native\Desktop\Facades\Window;
+
+class NativeAppServiceProvider implements ProvidesPhpIni
+{
+    /**
+     * Executed once the native application has been booted.
+     * Use this method to open windows, register global shortcuts, etc.
+     */
+    public function boot(): void
+    {
+        Window::open();
+    }
+
+    /**
+     * Return an array of php.ini directives to be set.
+     *
+     * @return array<string, bool|int|string>
+     */
+    public function phpIni(): array
+    {
+        return [];
+    }
+}
